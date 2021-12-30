4273b40010e3d26299156aa67bd7bed1ee62ec45
kushagra1999 <mishraa.kushagra@gmail.com>
2021-12-30 16:42:53 +0530

    Final Commit

--- myFramework/Content-Slider/CarouselTemplate.swift
+++ myFramework/Content-Slider/CarouselTemplate.swift
@@ -14,7 +14,7 @@ class CarouselTemplate: UIViewController, BaseTemplate {
     var myData: String?
     let urls: [URL] = [
         URL(string: "https://upload.wikimedia.org/wikipedia/en/2/21/Web_of_Spider-Man_Vol_1_129-1.png")!,
-        URL(string: "https://www.out.com/sites/default/files/andrew-garfield-comments-bisexual-spider-man-sony-apology-marvel-mcu.jpg")!,
+        URL(string: "https://cdn.vox-cdn.com/thumbor/bzPOaj6Cyr_s0X8BnzgnJ3s29fQ=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/22651022/TRA8660_105_TRL_comp_v004.1092.jpg")!,
         URL(string: "https://www.denofgeek.com/wp-content/uploads/2021/10/spider-man-no-way-home-iron-spider-tom-holland-sony.jpg?resize=768%2C432")!
     ]
     lazy var carousel = Carousel(frame: .zero, urls: urls)
